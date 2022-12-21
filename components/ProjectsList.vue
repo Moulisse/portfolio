b2b1400755893b72b1d10bb5c51f4c6b35898109
dlachartre <denis.lachartre@x-gil.fr>
2022-12-21 16:34:29 +0100

    a style + en savoir plus

--- components/ProjectsList.vue
+++ components/ProjectsList.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="lg:grid grid-cols-3 grid-flow-dense gap-y-32 gap-x-6 items-center"
+    class="lg:grid grid-cols-3 grid-flow-dense gap-y-48 lg:py-32 gap-x-6 items-center"
   >
     <template v-for="(project, i) in projects">
       <Blob :class="{ 'col-start-3': i % 2 !== 0 }">
@@ -13,11 +13,18 @@
       </Blob>
       <NuxtLink
         :href="project._path"
-        class="col-span-2 py-6"
+        class="col-span-2 py-6 px-4 group hover:bg-gray-50/5 hover:outline outline-2 outline-gray-50/10 rounded-xl transition"
         :class="{ 'col-start-2': i % 2 === 0 }"
       >
         <h2 class="text-3xl mb-6">{{ project.title }}</h2>
-        <div class="opacity-50 mb-8">{{ project.description }}</div>
+        <div class="mb-8">
+          <span class="opacity-50">{{ project.description }}</span
+          >&nbsp;
+          <span
+            class="underline lg:opacity-0 group-hover:!opacity-100 transition"
+            >En savoir plus
+          </span>
+        </div>
         <Stack
           :stack="project.stack"
           v-if="project.stack"
@@ -43,6 +50,4 @@ const { data: projects } = await useAsyncData("projects", () =>
     ])
     .find()
 );
-
-console.log(projects);
 </script>
